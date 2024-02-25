2fca07bd2de473a4aafac386d4106ca366596633
paulphillipsguru <contact@paulphillips.guru>
2024-02-25 11:14:30 +1100

    Moved dapper + interface to core, no longer depends on sperate nuget packages.

---
+++ src/PP.Framework.Core/Data/Dapper.cs
@@ -0,0 +1,14 @@
+﻿using PaulPhillips.Framework.Feature.Contracts;
+
+namespace PaulPhillips.Framework.Feature.Infrastructure.Dapper
+{
+    // NOTE: WIP, need to implement real dapper logic.
+    public class Dapper : IDataContract
+    {
+        public async Task<bool> RecordExistsAsync<T>(string tableName, string field, T value)
+        {
+            
+            return await Task.FromResult(false);
+        }
+    }
+}
